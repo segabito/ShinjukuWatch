1c4914a4017102e45a55eb6f2c11fc7fbcc1be4b
segabito <segabito@gmail.com>
2013-10-05 06:23:00 +0900

    - 動画切り替わり時にオススメを更新 - テレビちゃんメニューのスライドをなくした

--- ShinjukuWatch.user.js
+++ ShinjukuWatch.user.js
@@ -4,9 +4,16 @@
 // @description 原宿の後継バージョン的な
 // @include     http://www.nicovideo.jp/watch/*
 // @include     http://www.nicovideo.jp/mylist_add/video/*
-// @version     1.0.1
+// @version     1.1.0
 // @grant       none
 // ==/UserScript==
+
+// TODO: GINZAの動作チェック
+// ウォール機能どうする？
+
+// ver1.1.0
+// - 動画が切り替わる時にオススメ動画一覧を更新
+// - テレビちゃんメニューのスライドをなくした
 
 
 // ver1.0.0 最初のバージョン
@@ -15,6 +22,9 @@
   var monkey = (function(){
 
     window.Shinjuku = {
+      ns: {
+        loader: {}
+      }
     };
 
     if (!window.WatchApp) {
@@ -71,6 +81,26 @@
       return;
     }
 
+    window.Shinjuku.ns.loader.RelatedVideo = function() { this.initialize.apply(this, arguments); }
+    window.Shinjuku.ns.loader.RelatedVideo.prototype = {
+      initialize: function() {
+      },
+      load: function(watchId) {
+        var def = new $.Deferred();
+        window.WatchApp.ns.init.VideoExplorerInitializer.relatedVideoAPILoader.load(
+          {'video_id': watchId},
+          function(err, result) {
+            if (err !== null) {
+              return def.reject({message: '通信に失敗しました(1)', status: 'fail', err: err});
+            }
+            return def.resolve(result);
+          }
+        );
+        return def.promise();
+      }
+    };
+
+
     window.WatchApp.mixin(window.Shinjuku, {
       initialize: function() {
         if (window.WatchApp) {
@@ -86,6 +116,7 @@
         this.initializeVideoExplorer();
         this.initializeNicommend();
         this.initializeIchiba();
+        this.initializeOsusume();
         this.initializePlaylist();
         this.initializeQuickMylistFrame();
         this.initializeOther();
@@ -142,17 +173,16 @@
           body:not(.full_with_browser).size_normal #videoTagContainer {
             width: 1036px;
           }
-
+          body #videoTagContainer .tagInner #videoTagContainerPin.active {
+            display: none !important;
+          }
 
           {* 余白の除去 *}
           body #playerContainerWrapper { margin-top: -20px; }
-          #videoHeader.menuClosed #videoHeaderDetail {
-            margin-top: 0;
+          #videoHeader #videoHeaderDetail {
+            margin-top: 0 !important;
           }
-          body #videoHeaderDetail.active .videoDetailExpand {
-            height: 54px;
-            padding: 0;
-          }
+          #videoHeader .videoDetailExpand { height: auto !important; min-height: 32px;}
           #videoTagContainer { height: auto !important; }
           #videoTagContainer .tagInner {
             height: auto !important;
@@ -169,8 +199,15 @@
           .videoMenuToggle .tooltipOuter {
             display: none !important;
           }
+          .videoDetailToggleButton {
+            cursor: pointer;
+          }
+          .toggleDetailExpand, .shortVideoInfo {
+            display: none !important;
+          }
 
           #videoExplorerExpand, #playlist, #outline { display: none; }
+          #videoHeaderMenu .searchContainer { margin-top: 0px; }
 
           #outline .sidebar {
             float: none !important;
@@ -183,6 +220,8 @@
           }
           #outline #playerBottomAd {
             float: left: !important;
+            width: 300px !important; height: 256px !important;
+            overflow: hidden;
           }
           #outline #videoReviewBottomAd {
             float: right !important;
@@ -196,7 +235,8 @@
 
 
           {* 背景色 *}
-          #content { background: #fff; }
+          body, #outline { background: #f4f4f4; transition: background 1s ease 1s; }
+          body.Shinjuku,.Shinjuku #outline { background: #fff; }
           body #playerContainerWrapper { background: none; }
 
           {* 下半身いっぱいを市場表示 *}
@@ -211,6 +251,9 @@
             display: inline-block; float: none;
             text-align: left;
             vertical-align: top;
+          }
+          #ichibaMain #ichibaMainFooter .commandArea2 {
+            display: none; {* 変な位置に表示されてみっともないので *}
           }
           body.size_medium #ichibaMain dl {
             margin:0  9px 30px;
@@ -235,10 +278,20 @@
             bottom: 0px;
           }
           .osusumeContainer li  {
-            margin-bottom: 12px;
+            margin-bottom: 8px;
+            padding: 4px;
             border-bottom: 1px solid #ccc;
             text-align: left;
           }
+          .osusumeContainer .currentVideoRelated {
+            background: #fff;
+            font-weight: bolder;
+          }
+
+          .osusumeContainer .otherVideoRelated.first {
+            margin-top: 64px;
+          }
+
           .osusumeContainer .thumbnail img {
             float: left; width: 64px; height: 48px;
             margin-right: 4px;
@@ -261,15 +314,71 @@
           #content.noNews #textMarquee {
             display: none !important;
           }
-          body:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews #playerTabWrapper {
-            height: auto !important; position: absolute; bottom: 18px;
+
+          {* テレビちゃんメニュー スライドをやめる *}
+          body #videoHeader #videoMenuWrapper{
+            position: absolute; width: 324px; height: auto !important;
+            opacity: 0;
+            transition: opacity 0.4s ease;
+            right: 0px;
           }
-          body:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews              #playerTabContainer {
-            bottom: -17px;
+          body #videoHeader.menuOpened #videoMenuWrapper{
+            z-index: 1000 !important;
+            border: 1px solid #000;
+            background: white;
+            box-shadow: 0px 0px 4px #000;
+            top: auto;
+            bottom: 48px;
+            opacity: 1;
           }
-          body:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews .appli_panel #playerTabContainer {
-            bottom:  20px;
+          body #videoHeader #videoMenuWrapper .defmylistButton, body #videoHeader #videoMenuWrapper .mylistButton {
+            display: none !important;
           }
+          body #videoHeader #videoMenuTopList{
+            position: relative;
+            width: auto;
+          }
+          body #videoHeader.menuOpened #videoMenuWrapper .videoMenuList{
+            display: inline-block;
+            width: 60px;
+          }
+
+          {* テレビちゃんメニューのスライド殺す *}
+          body #videoHeader.menuOpened #videoMenuWrapper {
+            margin-bottom: 0;
+          }
+          body #videoHeader.menuOpened #videoHeaderDetail {
+            margin-top: 0px;
+          }
+
+          body.Shinjuku:not(.videoExplorer):not(.setting_panel):not(.full_with_browser)    #content.noNews              #playerTabWrapper {
+            height: auto !important; position: absolute;
+          }
+          body.size_medium:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews              #playerTabWrapper {
+            bottom: -33px;
+          }
+          body.size_medium:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews .appli_panel #playerTabWrapper {
+            bottom:  19px;
+          }
+          body.size_normal:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews              #playerTabWrapper {
+            bottom: 0px;
+          }
+          body.size_normal:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews .appli_panel #playerTabWrapper {
+            bottom: 18px;
+          }
+          body.size_medium:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews #playerAlignmentArea              #playerTabContainer {
+            bottom: 33px;
+          }
+          body.size_medium:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews #playerAlignmentArea .appli_panel #playerTabContainer {
+            bottom: 19px;
+          }
+          body.size_normal:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews #playerAlignmentArea              #playerTabContainer {
+            bottom:  0px;
+          }
+          body.size_normal:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews #playerAlignmentArea .appli_panel #playerTabContainer {
+            bottom: 19px;
+          }
+
           #playerTabWrapper.w_videoInfo #playerTabContainer, #playerTabWrapper.w_ichiba #playerTabContainer, #playerTabWrapper.w_review #playerTabContainer {
             bottom: 0px !important;
           }
@@ -279,9 +388,6 @@
           {
             height: auto !important; position: absolute; bottom: 2px;
           }
-         {* body:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews #leftPanel {
-            height: auto !important; position: absolute; bottom: 2px;
-          }*}
           body:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews #playerCommentPanel {
             height: 100% !important;
           }
@@ -290,20 +396,6 @@
           }
           body:not(.videoExplorer):not(.setting_panel):not(.full_with_browser) #content.noNews #playerContainer {
             height: auto;
-          }
-
-          {* なぜか :hover 使ってなくて見た目バグってるので :hover 使うようにする *}
-          body #playerCommentPanel .section .commentTable .commentTableContainer .cell:hover {
-            background: #5F5F5F;
-          }
-          body #playerCommentPanel .section .commentTable .commentTableContainer .cell:hover span {
-            color: #fff;
-          }
-          body #playerCommentPanel .section .commentTable .commentTableContainer .cell.hover {
-            background: none;
-          }
-          body #playerCommentPanel .section .commentTable .commentTableContainer .cell.hover span {
-            color: #000;
           }
 
           .quickMylistFrame.initialize {
@@ -317,13 +409,11 @@
             height: 24px;
             border: 0;
           }
-          .menuOpened .quickMylistFrame {
-            display: none;
-          }
 
           #videoInfo, #nicommendContainer, #videoReview {
             display: none !important;
           }
+
 
         */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1].replace(/\{\*/g, '/*').replace(/\*\}/g, '*/');
 
@@ -332,6 +422,9 @@
       initializeTag: function() {
         // タグ自動更新キャンセラー
         window.WatchApp.ns.model.player.NicoPlayerConnector.onTagDataRecieved = function() {};
+
+        // タグ領域のピン留め
+        window.WatchApp.ns.init.TagInitializer.tagViewController.tagViewPinStatus.changeStatus(true);
 
         var $a = $('.toggleTagEditInner a').detach();
         $('.toggleTagEditInner').empty().append($a);
@@ -402,6 +495,95 @@
         //  }
         //};
       },
+      initializeOsusume: function() {
+        // 動画が切り替わるたびに関連動画(オススメ)をリロードする
+        // でもYouTubeみたいに中身が全部入れ替わる方式だと「他に見たい奴もあったのに」を回収できなくて嫌
+        // なので、n件までたまっていく方式にする
+        var template = [
+          '<li class="%class%">',
+            '<a href="/watch/%videoId%" class="thumbnail"><img src="%thumbnail%"></a>',
+            '<a href="/watch/%videoId%" class="title">%title%</a>',
+            '<p>再: <span class="count">%view%</span>',
+            'コメ: <span class="count">%num_res%</span>',
+            'マイ: <span class="count">%mylist%</span></p>',
+          '</li>',
+        ''].join('');
+        var relatedVideo   = new window.Shinjuku.ns.loader.RelatedVideo({});
+        var watchInfoModel = this._watchInfoModel;
+        var MAX_ITEMS = 100;
+
+        var osusumeController = this.osusumeController = {
+          items: [],
+          $container: $('<div class="osusumeContainer" />'),
+          initialize: function() {
+            $('#nicommentPanelContainer').empty().append(this.$container);
+          },
+          add: function(item) {
+            item.baseId = watchInfoModel.v; // どの動画の関連だったか
+            this.items.unshift(item);
+            this.items.length = Math.min(this.items.length, 150);
+          },
+          addPlaylistItem: function(item) {
+            if (!item._hasData) return;
+            this.add({
+              id:             item.id,
+              thumbnail_url:  item.thumbnailUrl,
+              num_res:        item.numRes,
+              mylist_counter: item.mylistCounter,
+              view_counter:   item.viewCounter,
+              title:          item.title
+            });
+          },
+          refresh: function() {
+            var uniq = {}, count = 0, items = this.items;
+            var watchId = watchInfoModel.v;
+
+            var view = ['<ul>'];
+            for (var i = 0, len = items.length; i < len; i++) {
+              var item = items[i], id = item.id;
+
+              if (uniq[id] || watchId === id) continue;
+              uniq[id] = true;
+              if (++count > MAX_ITEMS) break;
+              var itemView = template
+                .split('%videoId%')  .join(item.id)
+                .split('%thumbnail%').join(item.thumbnail_url)
+                .split('%view%')     .join(item.view_counter)
+                .split('%num_res%')  .join(item.num_res)
+                .split('%mylist%')   .join(item.mylist_counter)
+                .split('%title%')    .join(item.title)
+                .split('%class%')    .join(item.baseId === watchId ? 'currentVideoRelated' : 'otherVideoRelated');
+              view.push(itemView);
+            }
+            view.push('</ul>');
+
+            this.$container.html(view.join('')).scrollTop(0).find('.otherVideoRelated:first').addClass('first');
+          }
+        };
+
+        osusumeController.initialize();
+
+        var update = function() {
+          relatedVideo.load(watchInfoModel.v).pipe(function(result) {
+            var items = result.list;
+            for (var i = items.length - 1; i >= 0; i--) {
+              if (items[i].type === 0 /* video */) osusumeController.add(items[i]);
+            }
+            osusumeController.refresh();
+          });
+        };
+
+        // 再生開始時にタブがコメントに変わるのはザッピングに邪魔なので切る
+        window.WatchApp.ns.init.PlayerInitializer.playerTab.playerAreaConnector.removeEventListener(
+          'onVideoStarted',
+          WatchApp.ns.init.PlayerInitializer.playerTab._onVideoStarted);
+
+        this._playerAreaConnector.addEventListener('onFirstVideoInitialized', $.proxy(function() {
+          watchInfoModel.addEventListener('reset', function() {
+            update();
+          });
+        }, this));
+      },
       initializePlaylist: function() {
         this.playlistController = {
           _playlist: window.WatchApp.ns.init.PlaylistInitializer.playlist,
@@ -436,37 +618,15 @@
         // プレイリストを空にする事で、プレーヤー上の「次の動画」「前の動画」ボタンを無効化して誤爆を防ぐことができる
         this.playlistController.clear();
 
-
-        // オススメタブの表示
-        // TODO: 動画が切り替わったらリロード
-        var tpl = [
-          '<li>',
-            '<a href="/watch/%videoId%" class="thumbnail"><img src="%thumbnail%"></a>',
-            '<a href="/watch/%videoId%" class="title">%title%</a>',
-            '<p>再: <span class="count">%view%</span>',
-            'コメ: <span class="count">%num_res%</span>',
-            'マイ: <span class="count">%mylist%</span></p>',
-          '</li>',
-        ''].join('');
-
-        var view = ['<ul>'];
-        for (var i = 0, len = Math.min(items.length, 16); i < len; i++) {
-          var item = items[i];
-          //console.log(JSON.stringify(item));
-          if (!item._hasData) continue;
-          var itemView = tpl
-            .split('%videoId%')  .join(item.id)
-            .split('%thumbnail%').join(item.thumbnailUrl)
-            .split('%view%')     .join(item.viewCounter)
-            .split('%num_res%')  .join(item.numRes)
-            .split('%mylist%')   .join(item.mylistCounter)
-            .split('%title%')    .join(item.title);
-          view.push(itemView);
+        // 通信回数を減らすため、
+        // 動画ページを開いた初回だけはrelatedVideoAPIではなく、プレイリストにあった動画をオススメにつっこむ。
+        // 投稿日時が取得できないのが難点
+        if (items.length > 0) {
+          for (var i = items.length - 1; i >= 0; i--) {
+            this.osusumeController.addPlaylistItem(items[i]);
+          }
+          this.osusumeController.refresh();
         }
-        view.push('</ul>');
-        var $container = $('<div class="osusumeContainer" />').html(view.join(''));
-        $('#nicommentPanelContainer').empty().append($container);
-
 
       },
       initializeIchiba: function() {
@@ -496,6 +656,7 @@
       initializeOther: function() {
         // $('#content').removeClass('panel_ads_shown'); // コメントパネルの広告消すやつ
         $('#content').addClass('noNews');
+        $('.videoDetailExpand h2').addClass('videoDetailToggleButton');
 
         var refreshTitle = function() {
           window.setTimeout(function() {
@@ -515,6 +676,8 @@
           }
         });
         this._playerAreaConnector.addEventListener('onFirstVideoInitialized', function() {
+          $('body').addClass('Shinjuku');
+
           // プレーヤーの位置に自動スクロール
 
           if (!$('#videoHeader').hasClass('infoActive')) {
